e7f05715923df3760a50e7785468dcf6bd9db79f
Roman <romazaniosdev@gmail.com>
2026-05-11 14:54:15 +0300

    final

--- 1AIMoodBoardPhotoApp/Utilities/AppLinks.swift
+++ 1AIMoodBoardPhotoApp/Utilities/AppLinks.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 enum AppLinks {
-    static let privacyPolicy = "https://example.com/privacy-policy"
-    static let termsOfUse = "https://example.com/terms-of-use"
+    static let privacyPolicy = "https://docs.google.com/document/d/10ERiQHU_wW3RmUEJ_9EIe46eGVqtByoMpSseKMbl9HY/edit?usp=sharing"
+    static let termsOfUse = "https://docs.google.com/document/d/1FhDwBEZTRQfAvBXu3rzxZ8B_X40GPfpmgBzHMVLiICE/edit?usp=sharing"
 }
