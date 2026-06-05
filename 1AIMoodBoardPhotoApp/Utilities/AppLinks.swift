007789143516adaa49e6b8ffe663aa450ebdb44e
Roman <romazaniosdev@gmail.com>
2026-06-05 16:39:22 +0300

    new terms

--- 1AIMoodBoardPhotoApp/Utilities/AppLinks.swift
+++ 1AIMoodBoardPhotoApp/Utilities/AppLinks.swift
@@ -7,5 +7,5 @@ import Foundation
 
 enum AppLinks {
     static let privacyPolicy = "https://docs.google.com/document/d/10ERiQHU_wW3RmUEJ_9EIe46eGVqtByoMpSseKMbl9HY/edit?usp=sharing"
-    static let termsOfUse = "https://docs.google.com/document/d/1FhDwBEZTRQfAvBXu3rzxZ8B_X40GPfpmgBzHMVLiICE/edit?usp=sharing"
+    static let termsOfUse = "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/"
 }
